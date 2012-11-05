22c9acda8ed8de71a37d94a1f471a02847574227
Ben L <nightgunner5@llamaslayers.net>
2012-11-04 20:59:31 -0600

    Remove Entity embed from supplemental interfaces

--- entity/supplemental.go
+++ entity/supplemental.go
@@ -1,19 +1,13 @@
 package entity
 
 type Positioner interface {
-	Entity
-
 	Position() (x, y, z float64)
 }
 
 type Healther interface {
-	Entity
-
 	Health() float64
 }
 
 type TakeDamager interface {
-	Entity
-
-	TakeDamage(amount float64, )
+	TakeDamage(amount float64, attacker Entity)
 }
