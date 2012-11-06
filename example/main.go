91552d3d5f91a2c636174643c42c6b4ff65b4f8e
Ben L <nightgunner5@llamaslayers.net>
2012-11-05 18:15:07 -0600

    yay spells

--- example/main.go
+++ example/main.go
@@ -2,31 +2,35 @@ package main
 
 import (
 	"github.com/Nightgunner5/gogame/entity"
+	"github.com/Nightgunner5/gogame/spell"
 	"log"
 	"math/rand"
 	"runtime"
 )
 
 const (
-	maxHealth    = 100
-	manaForSpell = 10
+	maxHealth = 100
+	maxMana   = 1000
 
-	castTime    = 1
-	spellDamage = 75
+	manaForDamageSpell = 10
+	castTime           = 1
+	spellDamage        = 75
 
-	healCastTime = 0.9
-	spellHealing = 25
+	manaForHealingSpell = 3
+	healCastTime        = 0.7
+	spellHealing        = 25
 )
 
 func variance() float64 {
-	return rand.Float64()/2 + 0.5
+	return rand.Float64()/2 + 0.75
 }
 
 type (
-	spellCast struct {
+	allPurposeSpell struct {
 		damage   float64
 		timeLeft float64
 		target   entity.EntityID
+		caster   entity.EntityID
 	}
 
 	Person interface {
@@ -40,12 +44,62 @@ type (
 	person struct {
 		entity.EntityID
 
+		spell.SpellCaster
+
 		x, y, z float64
 		health  float64
 		mana    float64
-		spell   *spellCast
 	}
 )
+
+func (s *allPurposeSpell) Tick(delta float64) bool {
+	if s.timeLeft <= 0 {
+		return true
+	}
+	s.timeLeft -= delta
+	if s.timeLeft <= 0 {
+		target, caster := s.Target(), s.Caster()
+		if caster == nil {
+			log.Printf("%d: Spell failed: You are dead", s.caster)
+			return true
+		}
+		if target == nil {
+			log.Printf("%d: Spell failed: Target is dead", s.caster)
+			return true
+		}
+		if s.damage == 0 {
+			return true
+		}
+		if s.damage > 0 {
+			log.Printf("%d: Hit %d for %0.1f damage", s.caster, s.target, s.damage)
+		} else {
+			log.Printf("%d: Healed %d for %0.1f", s.caster, s.target, -s.damage)
+		}
+		target.(entity.TakeDamager).TakeDamage(s.damage, caster)
+		return true
+	}
+	return false
+}
+
+func (s *allPurposeSpell) Target() entity.Entity {
+	return entity.Get(s.target)
+}
+
+func (s *allPurposeSpell) Caster() entity.Entity {
+	return entity.Get(s.caster)
+}
+
+func (s *allPurposeSpell) Interrupt() bool {
+	if s.timeLeft > 0 {
+		s.timeLeft = -1
+		return true
+	}
+	return false
+}
+
+func (s *allPurposeSpell) TimeLeft() float64 {
+	return s.timeLeft
+}
 
 func (p *person) Health() float64 {
 	return p.health
@@ -67,6 +121,7 @@ func (p *person) TakeDamage(amount float64, attacker entity.Entity) {
 	if p.health > maxHealth {
 		p.health = maxHealth
 	}
+	p.Interrupt()
 	if p.health <= 0 {
 		log.Printf("%d: Killed by %d", p.ID(), attacker.ID())
 		p.health = 0
@@ -76,56 +131,52 @@ func (p *person) TakeDamage(amount float64, attacker entity.Entity) {
 
 func (p *person) Think(delta float64) {
 	p.mana += delta
+	if p.mana > maxMana {
+		p.mana = maxMana
+	}
 
-	//log.Printf("%d H:%f M:%f", p.ID(), p.health, p.mana)
+	if p.SpellCaster.Tick(delta) {
+		// do nothing; spell is casting
+	} else if p.Health() < maxHealth-spellHealing {
+		if p.mana >= manaForHealingSpell {
+			log.Printf("%d: H%d M%d", p.ID(), int(p.Health()), int(p.mana))
 
-	if p.spell != nil {
-		p.spell.timeLeft -= delta
-		if p.spell.timeLeft <= 0 {
-			target := entity.Get(p.spell.target)
-			if target == nil {
-				log.Printf("%d: Spell failed: Target is dead", p.ID())
-			} else {
-				log.Printf("%d: Hit %d with a spell", p.ID(), target.ID())
-				target.(entity.TakeDamager).TakeDamage(p.spell.damage, p)
-			}
-			p.spell = nil
-		}
-	} else if p.mana >= manaForSpell {
-		var target entity.Entity
-		// Yay nondeterminism
-		entity.ForAllNearby(p, 1, func(e entity.Entity) {
-			if _, ok := e.(entity.TakeDamager); ok {
-				if _, ok := e.(entity.Healther); ok {
-					target = e
-				}
-			}
-		})
+			p.mana -= manaForHealingSpell
 
-		if target == nil {
-			log.Printf("%d: Spell failed: No target", p.ID())
-			notarget <- true
-			return
-		}
-
-		p.mana -= manaForSpell
-
-		if target.(entity.Healther).Health() <= p.Health() {
-			p.spell = &spellCast{
-				damage:   spellDamage * variance(),
-				target:   target.ID(),
-				timeLeft: castTime,
-			}
-		} else {
-			p.spell = &spellCast{
+			p.Cast(&allPurposeSpell{
 				damage:   -spellHealing * variance(),
 				target:   p.ID(),
+				caster:   p.ID(),
 				timeLeft: healCastTime,
-			}
+			})
+			log.Printf("%d: Started healing self", p.ID())
 		}
-		log.Printf("%d: Started casting spell on %d\n", p.ID(), target.ID())
 	} else {
-		//log.Printf("%d: Spell failed: Not enough mana", p.ID())
+		if p.mana >= manaForDamageSpell {
+			var target entity.Entity
+			// Yay nondeterminism
+			entity.ForAllNearby(p, 1, func(e entity.Entity) {
+				if _, ok := e.(Person); ok {
+					target = e
+				}
+			})
+
+			if target == nil {
+				log.Printf("%d: Spell failed: No target", p.ID())
+				notarget <- true
+				return
+			}
+
+			p.mana -= manaForDamageSpell
+
+			p.Cast(&allPurposeSpell{
+				damage:   spellDamage * variance(),
+				target:   target.ID(),
+				caster:   p.ID(),
+				timeLeft: castTime,
+			})
+			log.Printf("%d: Started casting spell on %d", p.ID(), target.ID())
+		}
 	}
 }
 
---
+++ spell/spell.go
@@ -0,0 +1,50 @@
+package spell
+
+import "github.com/Nightgunner5/gogame/entity"
+
+type Spell interface {
+	// Returns true if the spell has ended, false if it has not.
+	Tick(delta float64) bool
+
+	Caster() entity.Entity
+	Target() entity.Entity
+
+	TimeLeft() float64
+
+	// Interrupts the spell and returns true. If the spell is not
+	// interruptable, returns false and does nothing.
+	Interrupt() bool
+}
+
+type Caster interface {
+	Interrupt() bool
+}
+
+type SpellCaster struct {
+	current Spell
+}
+
+func (c *SpellCaster) Tick(delta float64) bool {
+	spell := c.current
+	if spell == nil {
+		return false
+	}
+	if spell.Tick(delta) {
+		c.current = nil
+		return false
+	}
+	return true
+}
+
+func (c *SpellCaster) Cast(spell Spell) {
+	c.current = spell
+}
+
+func (c *SpellCaster) Interrupt() bool {
+	spell := c.current
+	if spell != nil && spell.Interrupt() {
+		c.current = nil
+		return true
+	}
+	return false
+}
