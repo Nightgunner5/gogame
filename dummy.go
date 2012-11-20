a642db57e982b57d4c3fffbf772c383715a253eb
Ben L <nightgunner5@llamaslayers.net>
2012-11-20 13:08:01 -0600

    add package effect to the dummy.go list

--- dummy.go
+++ dummy.go
@@ -1,6 +1,7 @@
 package dummy
 
 import (
+	_ "github.com/Nightgunner5/gogame/effect"
 	_ "github.com/Nightgunner5/gogame/entity"
 	_ "github.com/Nightgunner5/gogame/network"
 	_ "github.com/Nightgunner5/gogame/spell"
