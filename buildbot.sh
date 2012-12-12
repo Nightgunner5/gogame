a4a8bf35926942a8d0c9ba70eec407e1fd7aa7a5
Ben L <nightgunner5@llamaslayers.net>
2012-12-12 13:18:58 -0600

    fix dependency getting for buildbot script

--- buildbot.sh
+++ buildbot.sh
@@ -8,6 +8,6 @@ TAGS=$3
 
 go env | export
 
-go get -d || exit $?
+go get -d github.com/Nightgunner5/gogame/main || exit $?
 go test -v -race github.com/Nightgunner5/gogame/... || exit $?
 go build -x -o gogame$GOEXT -tags "$TAGS" github.com/Nightgunner5/gogame/main || exit $?
