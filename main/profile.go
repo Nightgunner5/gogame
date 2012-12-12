2f7109c61ea1eeedcb9ed1b1e3e57b830c3bd717
Ben L <nightgunner5@llamaslayers.net>
2012-12-12 15:46:50 -0600

    profiling mode

---
+++ main/profile.go
@@ -0,0 +1,52 @@
+// +build profile,!noserver,noclient
+
+package main
+
+import (
+	"os"
+	"os/signal"
+	"runtime/pprof"
+	"time"
+)
+
+func init() {
+	f, err := os.Create("cpu.prof")
+	if err != nil {
+		panic(err)
+	}
+
+	err = pprof.StartCPUProfile(f)
+	if err != nil {
+		panic(err)
+	}
+
+	cleanup := make(chan os.Signal, 1)
+
+	go func() {
+		tick := time.Tick(time.Minute)
+		for {
+			select {
+			case <-cleanup:
+				pprof.StopCPUProfile()
+				os.Exit(0)
+			case <-tick:
+				for _, profile := range pprof.Profiles() {
+					f, err := os.Open(profile.Name() + ".prof")
+					if err != nil {
+						panic(err)
+					}
+					err = profile.WriteTo(f, map[string]int{
+						"threadcreate": 1,
+						"goroutine":    2,
+					}[profile.Name()])
+					if err != nil {
+						panic(err)
+					}
+					f.Close()
+				}
+			}
+		}
+	}()
+
+	signal.Notify(cleanup, os.Interrupt)
+}
