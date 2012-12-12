f8602df13a7cdb68462e1abe4e5edc46d43fbf48
Ben L <nightgunner5@llamaslayers.net>
2012-12-12 13:11:19 -0600

    new buildbot script

---
+++ buildbot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export GOPATH=$PWD/gopath
+
+export GOOS=$1
+export GOARCH=$2
+TAGS=$3
+
+go env | export
+
+go get -d || exit $?
+go test -v -race github.com/Nightgunner5/gogame/... || exit $?
+go build -x -o gogame$GOEXT -tags "$TAGS" github.com/Nightgunner5/gogame/main || exit $?
