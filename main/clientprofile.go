5971fe5a932ed5e97a4a6b1f033af3e40ca76dfa
Ben L <nightgunner5@llamaslayers.net>
2012-12-15 17:46:07 -0600

    this time without syntax errors

--- main/clientprofile.go
+++ main/clientprofile.go
@@ -3,15 +3,13 @@
 package main
 
 import (
-	"os"
-
 	clientpkg "github.com/Nightgunner5/gogame/client"
 )
 
 func init() {
 	oldDisconnected := clientpkg.Disconnected
 	clientpkg.Disconnected = func() {
-		profileCleanup <- 0
+		profileCleanup <- nil
 		oldDisconnected()
 	}
 }
