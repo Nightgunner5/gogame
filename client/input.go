b9d871178fd26ac94f497a419a588e79218a3cc4
Ben L <nightgunner5@llamaslayers.net>
2012-12-15 16:16:14 -0600

    client: support WASD and the equivelent on Dvorak

--- client/input.go
+++ client/input.go
@@ -17,25 +17,35 @@ func init() {
 	keyDown[wde.KeyLeftArrow] = updateMotion
 	keyDown[wde.KeyRightArrow] = updateMotion
 
+	keyDown[wde.KeyW] = updateMotion
+	keyDown[wde.KeyS] = updateMotion
+	keyDown[wde.KeyA] = updateMotion
+	keyDown[wde.KeyD] = updateMotion
+
 	keyUp[wde.KeyUpArrow] = updateMotion
 	keyUp[wde.KeyDownArrow] = updateMotion
 	keyUp[wde.KeyLeftArrow] = updateMotion
 	keyUp[wde.KeyRightArrow] = updateMotion
+
+	keyUp[wde.KeyW] = updateMotion
+	keyUp[wde.KeyS] = updateMotion
+	keyUp[wde.KeyA] = updateMotion
+	keyUp[wde.KeyD] = updateMotion
 }
 
 func updateMotion(keys map[string]bool) {
 	dx, dy := 0, 0
 
-	if keys[wde.KeyUpArrow] {
+	if keys[wde.KeyUpArrow] || keys[wde.KeyW] || keys[wde.KeyComma] {
 		dy--
 	}
-	if keys[wde.KeyDownArrow] {
+	if keys[wde.KeyDownArrow] || keys[wde.KeyS] || keys[wde.KeyO] {
 		dy++
 	}
-	if keys[wde.KeyLeftArrow] {
+	if keys[wde.KeyLeftArrow] || keys[wde.KeyA] {
 		dx--
 	}
-	if keys[wde.KeyRightArrow] {
+	if keys[wde.KeyRightArrow] || keys[wde.KeyD] || keys[wde.KeyE] {
 		dx++
 	}
 
